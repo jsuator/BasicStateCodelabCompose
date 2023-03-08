c43bae0066733a6d9fe681ff3fb5f1dc963b4de6
javier.suarez <javier.suarez@s-dos.es>
2023-03-08 20:24:14 +0100

    savepoint step 7 before delete example code

--- app/src/main/java/com/example/basicstatecodelabcompose/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelabcompose/WaterCounter.kt
@@ -1,16 +1,50 @@
 package com.example.basicstatecodelabcompose
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You have $count beers.",
-        modifier = modifier.padding(16.dp)
-    )
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember {
+            mutableStateOf(0)
+        }
+
+        if (count > 0) {
+            var showTask by remember {
+                mutableStateOf(true)
+            }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken 15 minute walk today",
+                    onClose = { showTask = false })
+            }
+            Text(
+                text = "You have ${count} beers.",
+                modifier = modifier.padding(16.dp)
+            )
+        }
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            ) {
+                Text("Give me another")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp),
+                enabled = count < 10
+            ) {
+                Text("Go to toilet")
+            }
+        }
+    }
+
 }
