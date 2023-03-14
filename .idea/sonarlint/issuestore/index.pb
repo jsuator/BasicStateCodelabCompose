b09a6647771d0aba693083ce66af6b39776780b8
javier.suarez <javier.suarez@s-dos.es>
2023-03-14 19:41:33 +0100

    step 12

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+x
+Happ/src/main/java/com/example/basicstatecodelabcompose/ui/theme/Theme.kt,b/f/bf981c58f03f361273d33ee1aff64838818bbe5c
--- app/src/main/java/com/example/basicstatecodelabcompose/WellnessTask.kt
+++ app/src/main/java/com/example/basicstatecodelabcompose/WellnessTask.kt
@@ -1,4 +1,15 @@
 package com.example.basicstatecodelabcompose
 
-data class WellnessTask(val id: Int, val label: String)
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+data class WellnessTask(
+    val id: Int,
+    val label: String,
+    val initialChecked: Boolean = false
+) {
+
+    var checked by mutableStateOf(initialChecked)
+}
 
